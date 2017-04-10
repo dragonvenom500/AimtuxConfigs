bb6c02ad888e7857fbc51e0a165b57bc284001c5
dragonvenom500 <samendicott@outlook.com>
2017-04-10 16:21:31 -0400

    stole floody's cfg, and added grenade helper configs

--- install.sh
+++ install.sh
@@ -5,6 +5,10 @@
 if [ ! -d ~/.config/AimTux ]; then
     mkdir ~/.config/AimTux/
 fi
+if [ ! -d ~/.config/AimTuxGH ]; then
+    mkdir ~/.config/AimTuxGH/
+fi
 cp -ar configs/* ~/.config/AimTux/
+cp -ar grenadeconfigs/* ~/.config/AimTuxGH/
 echo Done!
 echo If CSGO is already open, press the reload button!
